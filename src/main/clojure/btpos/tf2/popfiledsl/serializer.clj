1d1d9bd729aeec9560ea7b188008c67ac6c8ca4d
ByThePowerOfScience <16433721+ByThePowerOfScience@users.noreply.github.com>
2026-06-08 18:45:45 -0400

    Update serialization again to handle the different key formats required

--- src/main/clojure/btpos/tf2/popfiledsl/serializer.clj
+++ src/main/clojure/btpos/tf2/popfiledsl/serializer.clj
@@ -1,33 +1,69 @@
 (ns btpos.tf2.popfiledsl.serializer
-  (:import (btpos.tf2.popfiledsl.serialization IPopFileRepresentable PopFileEntry PopFileMap PopFileQuotedString)))
+  (:require [clojure.string :as str])
+  (:import (btpos.tf2.popfiledsl.serialization IPopFileRepresentable PopFileEntry PopFileMap PopFileLiteralString)
+           (clojure.lang ISeq)
+           (java.util Collection)))
 
 
+
+(derive Collection ::collection)
+(derive ISeq ::collection)
+
+; Serialize some item to a popfile form
 (defmulti pop-file-serialize (fn [item] (class item)))
 
-; Primitives
+; Different kinds of entries do need to be formatted differently
+(defmulti pop-file-serialize-entry (fn [_key value] (type (.getValue value))))
+
+
+
+; default, just `toString` it
 (defmethod pop-file-serialize :default [item]
   (str item))
 
-(defmethod pop-file-serialize PopFileQuotedString [^PopFileQuotedString item]
+; string literals need to be quoted, but any other primitive (including normal strings) can be pasted as-is
+(defmethod pop-file-serialize PopFileLiteralString [^PopFileLiteralString item]
   (str \" (.string item) \"))
 
-
+; get the representation from the item
 (defmethod pop-file-serialize IPopFileRepresentable [^IPopFileRepresentable item]
   (pop-file-serialize (.getPopFileRepr item)))
 
+; raw lists should have their elements separated by spaces,
+; though I'm wondering if this is necessary since we handle collections through `pop-file-serialize-entry`
+; TODO
+(defmethod pop-file-serialize Collection [^Collection item]
+  (->> item
+       (map pop-file-serialize)
+       (str/join " ")))
 
-; Really, everything is just identifier-value pairs, it's just that structs have their own identifiers and values tend not to
-(defmethod pop-file-serialize PopFileEntry [^PopFileEntry pair]
-  (str (.getKey pair) (if (instance? PopFileMap (.getValue pair)) "\n" " ") (.getValue pair)))
 
+
+; Really, everything is just identifier-value pairs, it's just that structs have their own identifiers.
+(defmethod pop-file-serialize PopFileEntry [^PopFileEntry entry]
+  (pop-file-serialize-entry (.getKey entry) (.getValue entry)))
+
+; surround maps' elements with braces
 (defmethod pop-file-serialize PopFileMap [^PopFileMap subtree]
   (str
-    "{"
-    (->> (.getEntries subtree)
-         (map pop-file-serialize)
-         (str "\n"))
+    "{\n"
+    (pop-file-serialize (.getEntries subtree))
     "\n}"))
 
 
+
+; ===== FORMATTING ENTRIES =====
+
+(defmethod pop-file-serialize-entry PopFileMap [key value]
+  (str (pop-file-serialize key) "\n"
+       (pop-file-serialize value)))
+
+; Convert a list of [key [value1 value2 value3]] into a [[key value1] [key value2] ...]
+(defmethod pop-file-serialize-entry ::collection [key ^Collection value]
+  (pop-file-serialize (map #(.PopFileEntry key %) (seq value))))
+
+; If the value isn't a list we need to destructure or a map we need to offset with a newline, just write both normally
+(defmethod pop-file-serialize-entry :default [key value]
+  (str (pop-file-serialize key) " " (pop-file-serialize value)))
 
 
