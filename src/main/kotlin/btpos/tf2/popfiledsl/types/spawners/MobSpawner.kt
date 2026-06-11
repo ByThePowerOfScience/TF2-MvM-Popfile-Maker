b4ecf2f90ab0a83336ecf157ccb60231d4792a94
ByThePowerOfScience <16433721+ByThePowerOfScience@users.noreply.github.com>
2026-06-11 13:28:46 -0400

    Fix MobSpawner accidentally supporting multiple spawners

--- src/main/kotlin/btpos/tf2/popfiledsl/types/spawners/MobSpawner.kt
+++ src/main/kotlin/btpos/tf2/popfiledsl/types/spawners/MobSpawner.kt
@@ -1,16 +1,20 @@
 package btpos.tf2.popfiledsl.types.spawners
 
+import btpos.tf2.popfiledsl.modeling.IMvMSubtree.Companion.singleKeyedValue
+import btpos.tf2.popfiledsl.modeling.IMvMSubtree.Companion.singleStruct
+
 @Deprecated("According to sigsegv: \"Old and crusty\"")
-class MobSpawner(count: Int, spawners: List<BaseSpawner>) : BaseSpawner() {
-	var count: Int? by singleKeyedValue("Count")
-	val spawners: MutableList<BaseSpawner> by multiStruct()
-	
-	init {
-		this.count = count
-		this.spawners.clear()
-		this.spawners.addAll(spawners)
-	}
-	
+class MobSpawner : BaseSpawner() {
 	override val popFileStructIdentifier: Any
 		get() = "Mob"
+}
+
+var MobSpawner.count: Int? by singleKeyedValue("Count")
+var MobSpawner.spawner: BaseSpawner? by singleStruct()
+
+fun MobSpawner(count: Int, spawner: BaseSpawner): MobSpawner {
+	return MobSpawner().apply {
+		this.count = count
+		this.spawner = spawner
+	}
 }
