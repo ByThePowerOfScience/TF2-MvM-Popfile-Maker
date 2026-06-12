d84e25e7cc2691af81dc6b8c9a44e4e71ee4fd39
ByThePowerOfScience <16433721+ByThePowerOfScience@users.noreply.github.com>
2026-06-12 10:04:20 -0400

    Un-hardcode NavArea's literal string type

--- src/main/kotlin/btpos/tf2/popfiledsl/types/specifics/NavArea.kt
+++ src/main/kotlin/btpos/tf2/popfiledsl/types/specifics/NavArea.kt
@@ -3,16 +3,13 @@ package btpos.tf2.popfiledsl.types.specifics
 import btpos.tf2.popfiledsl.serialization.IPopFileSerializable
 import btpos.tf2.popfiledsl.serialization.PopFileStringLiteral
 
-class NavArea(val name: String) : IPopFileSerializable<PopFileStringLiteral> {
-	override val _popFileRepr: PopFileStringLiteral
-		get() = PopFileStringLiteral(name)
-	
+class NavArea(override val _popFileRepr: PopFileStringLiteral) : IPopFileSerializable<PopFileStringLiteral> {
 	companion object
 }
 
 val NavArea.Companion.SENTRY_SPOT
-	get() = NavArea("SENTRY_SPOT")
+	get() = NavArea(PopFileStringLiteral("SENTRY_SPOT"))
 
 val NavArea.Companion.SNIPER_SPOT
-	get() = NavArea("SNIPER_SPOT")
+	get() = NavArea(PopFileStringLiteral("SENTRY_SPOT"))
 
