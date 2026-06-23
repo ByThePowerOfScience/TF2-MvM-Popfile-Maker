0d57d3beed2b258ba0c3a4fb223a46b577145bbc
ByThePowerOfScience <16433721+ByThePowerOfScience@users.noreply.github.com>
2026-06-23 13:26:47 -0400

    Very intelligent overengineered parser combinators to learn how to make them

--- ItemAttributesGeneration/src/main/clojure/btpos/tf2/popfiledsl/parsercombinator.clj
+++ ItemAttributesGeneration/src/main/clojure/btpos/tf2/popfiledsl/parsercombinator.clj
@@ -1,11 +1,15 @@
 (ns btpos.tf2.popfiledsl.parsercombinator
-  (:import (clojure.lang StringSeq)
-           (java.util.regex Matcher)))
+  (:import (clojure.lang StringSeq))
+  (:refer-clojure :exclude [map empty or partial]))
 
 (defn result [item ^StringSeq rest-of-seq]
   [item rest-of-seq])
 
+(defn partial [reason item ^StringSeq rest-of-seq]
+  (result (conj item [[:partial true] [:reason reason]]) rest-of-seq))
 
+(defn partial? [[item, _seq]]
+  (:partial item))
 
 (def empty
   "Matches the empty string."
@@ -23,28 +27,37 @@
 
 (def chartoliteral #(if (char? %) (terminal %) %))
 
+(defn flatten-partials [partials]
+  (reduce
+    (fn [acc, el]
+      (conj acc (el 0)))
+    []
+    partials))
 
 (defn or
   "Parser combinator, does a buncha fuckin stuff idfk whatever"
   [& parsers]
-  (let [parsers (map chartoliteral parsers)]
+  (let [parsers (clojure.core/map chartoliteral parsers)]
     (fn [^StringSeq input]
-      (set (->> (seq parsers)
-                (map #(apply % input))
-                (keep identity))))))
+      (let [postapply (->> (seq parsers) (clojure.core/map #(apply % input)))]
+        (set ((if
+          (clojure.core/every? :partial postapply)
+          flatten-partials
+          (filter #(not (:partial %)))) postapply))))))
 
 
 (defn then
   [& parsers]
-  (let [parsers (map chartoliteral parsers)]
+  (let [parsers (clojure.core/map chartoliteral parsers)]
     (fn [input]
-      (reduce
-        (fn [[previtems prevrest], curr_parser]
-          (if-some [[ouritem ourrest] (curr_parser prevrest)]
-            (result (conj previtems ouritem) ourrest)
-            (reduced nil)))
-        [[] input]
-        (seq parsers)))))
+      (loop [previtems []
+             nextseq input
+             parsers parsers]
+        (if-some [curr_parser (first parsers)]
+          (if-some [[ouritem ourrest] (curr_parser nextseq)]
+            (recur (conj previtems ouritem) ourrest (rest parsers))
+            (partial ::FAIL { :type :list :value previtems } nextseq))
+          (result { :type :list :value previtems } nextseq))))))
 
 
 
@@ -58,13 +71,14 @@
       )
   ))
 
-(defn parsermap [mapper parser]
+
+(defn map [mapper parser]
   (fn [^StringSeq input]
     (when-some [[item, ^StringSeq seq] (parser input)]
       (result (mapper item) seq))))
 
 (defn plus [parser]
-  (parsermap
+  (map
     #({ :type :list
        :value (let [items (:value %)]
                 (vec (cons (items 0) (items 1)))) }) (then parser (star parser))))
@@ -74,12 +88,13 @@
     (fn [^StringSeq input]
       (loop [currseq input
              curritems (clojure.core/empty [])]
-        (when-some [currseq (seq currseq)]
+        (if-some [currseq (seq currseq)]
           (let [^Character currentchar (first currseq)]
             (if (charpredicate currentchar)
               (recur (rest currseq) (conj curritems currentchar))
-              (result { :type :string :value (apply str curritems) } currseq))))))))
+              (result { :type :string :value (apply str curritems) } currseq)))
+          (partial ::EOF { :type :string :value (apply str curritems) } currseq))))))
 
-(def word (takewhile #(Character/isLetterOrDigit (.charValue %))))
+(def word (takewhile (fn [^Character c] (Character/isLetterOrDigit (.charValue c)))))
 
 (defn take-until [^Character c] (takewhile #(not (= c %))))
--- ItemAttributesGeneration/src/main/clojure/btpos/tf2/popfiledsl/wikitableparser.clj
+++ ItemAttributesGeneration/src/main/clojure/btpos/tf2/popfiledsl/wikitableparser.clj
@@ -9,6 +9,8 @@
 ; V -> "=" (V1 | T)
 ; V1 -> [^|]+
 
+
+
 (defn table [])
 
 (def V1
@@ -35,3 +37,5 @@
         V))
     double-close-bracket))
 
+(def thing "{{Item Attribute|id=1|name=damage penalty|description={{lang|en='''%s1'''% damage penalty|da='''%s1'''% skadestraf|de='''%s1'''% geringerer Schaden|es='''%s1''' % de daño|fi='''%s1'''% vahinkovähennys|fr=Pénalité de dégâts '''%s1'''%|hu='''%s1'''% sebzéscsökkentés|it='''%s1'''% di riduzione dei danni|ja=与ダメージ '''%s1'''%|ko=피해량 '''%s1'''% 감소|nl='''%s1'''% toegebrachte schade|no='''%s1'''% skadestraff|pl='''%s1'''% zadawanych obrażeń|pt=Penalidade de '''%s1'''% de dano|pt-br=Dano causado '''%s1'''% menor|ro='''%s1'''% penalizare de damage|ru=Урон: '''%s1'''%|sv='''%s1'''% mindre skada|tr='''%s1'''% hasar verir|zh-hans=伤害 -'''%s1'''%|zh-hant=傷害值 '''%s1'''%}}|value-type=percentage|class=mult_dmg|effect-type=negative|notes=}}")
+(defn parse-table [^String input] ((table) (seq input)))
