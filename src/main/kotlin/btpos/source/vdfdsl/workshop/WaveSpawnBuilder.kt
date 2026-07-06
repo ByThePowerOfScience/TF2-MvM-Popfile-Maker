98927a4a6efea30159e2113e2e3205fc4f7f426f
ByThePowerOfScience <16433721+ByThePowerOfScience@users.noreply.github.com>
2026-07-06 17:40:13 -0400

    Save idea: infix builder for wave spawns (not likely to implement, but it IS cool)

---
+++ src/main/kotlin/btpos/source/vdfdsl/workshop/WaveSpawnBuilder.kt
@@ -0,0 +1,55 @@
+package btpos.source.vdfdsl.workshop
+
+import btpos.source.vdfdsl.types.populators.WaveSpawnPopulator
+import btpos.source.vdfdsl.types.populators.provideDelegate
+import btpos.source.vdfdsl.types.spawners.AbstractSpawner
+import btpos.source.vdfdsl.types.spawners.TFBot
+import btpos.source.vdfdsl.utils.toSeconds
+import kotlin.reflect.KProperty
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
+
+/**
+ * Showcase of another possible syntax: infix "sentence-like" grammar.
+ *
+ * I'm not a fan, but it _is_ cool that you can do this without any allocations.
+ */
+@Suppress("NOTHING_TO_INLINE")
+@JvmInline value class WaveSpawnBuilder(val internal: WaveSpawnPopulator) {
+	/**
+	 * Spawn the bots after this much time has elapsed since the start of the wave.
+	 *
+	 * @see Duration.Companion.seconds
+	 */
+	inline infix fun after(timing: Duration) = WaveSpawnBuilder(internal.apply {
+		waitBeforeStarting = timing.toSeconds()
+	})
+	
+	inline infix fun afterFinishSpawning(subwave: WaveSpawnPopulator) = WaveSpawnBuilder(internal.apply {
+		waitForAllSpawned = subwave
+	})
+	
+	inline infix fun afterBeating(subwave: WaveSpawnPopulator) = WaveSpawnBuilder(internal.apply {
+		waitForAllDead = subwave
+	})
+	
+	inline infix fun inGroupsOf(number: Int) = WaveSpawnBuilder(internal.apply {
+		maxActive = number
+		spawnCount = number
+	})
+	
+	inline infix fun waitingBetweenSpawns(timing: Duration) = WaveSpawnBuilder(internal.apply {
+		waitBetweenSpawns = timing.toSeconds()
+	})
+	
+	inline operator fun provideDelegate(thisRef: Any?, prop: KProperty<*>) = internal.provideDelegate(thisRef, prop)
+}
+
+fun spawn(number: Int, spawner: AbstractSpawner) = WaveSpawnBuilder(WaveSpawnPopulator().apply {
+	totalCount = number
+	this.spawner = spawner
+})
+
+fun foo() {
+	val wave01b by spawn(2, TFBot(template="foo")) after 2.seconds inGroupsOf 1 waitingBetweenSpawns 2.seconds
+}
