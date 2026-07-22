ee301e7f7d83607822db6078341622171ea3b43e
ByThePowerOfScience <16433721+ByThePowerOfScience@users.noreply.github.com>
2026-07-22 13:08:58 -0400

    Main: Make IKeyValueMap always keyed by String

--- vdf-modeling/src/main/kotlin/btpos/source/vdfdsl/modeling/KeyValueMapImpl.kt
+++ vdf-modeling/src/main/kotlin/btpos/source/vdfdsl/modeling/KeyValueMapImpl.kt
@@ -1,9 +1,7 @@
 package btpos.source.vdfdsl.modeling
 
-import btpos.source.vdfdsl.backing.VDFKeyValue
 import btpos.source.vdfdsl.backing.VDFPrimitive
 import btpos.source.vdfdsl.backing.VDFSubtree
-import btpos.source.vdfdsl.serialization.IVDFRepresentableKeyValue
 import btpos.source.vdfdsl.serialization.IVDFRepresentableValue
 import btpos.source.vdfdsl.serialization.IVDFRepresentableValue_Subtree
 import btpos.source.vdfdsl.serialization.codecs.Codec
@@ -13,17 +11,21 @@ import kotlin.collections.set
  * Only allows a single instance of each key in the map.
  */
 interface IKeyValueMap {
-	fun <T> getTyped(key: Any): T?
+	fun <T> getTyped(key: String): T? {
+		return getTyped(key, Codec.identity())
+	}
 	
-	fun setNullable(key: Any, value: Any?)
+	fun setNullable(key: String, value: Any?) {
+		return setNullable(key, value, Codec.identity())
+	}
 	
-	operator fun set(key: Any, value: Any?) = setNullable(key, value)
+	operator fun set(key: String, value: Any?) = setNullable(key, value)
 	
-	operator fun <T> get(key: Any) = getTyped<T>(key)
+	operator fun <T> get(key: String) = getTyped<T>(key)
 	
-	fun <FRONTEND : Any> getTyped(key: Any, codec: Codec<FRONTEND, Any>): FRONTEND?
+	fun <FRONTEND : Any> getTyped(key: String, codec: Codec<FRONTEND, Any>): FRONTEND?
 	
-	fun <FRONTEND : Any> setNullable(key: Any, value: FRONTEND?, codec: Codec<FRONTEND, Any>)
+	fun <FRONTEND : Any> setNullable(key: String, value: FRONTEND?, codec: Codec<FRONTEND, Any>)
 }
 
 
@@ -39,22 +41,22 @@ open class KeyValueMapImpl(private val _attributes: MutableMap<Any, Any> = mutab
 	}
 	
 	@Suppress("UNCHECKED_CAST")
-	override fun <T> getTyped(key: Any): T? {
+	override fun <T> getTyped(key: String): T? {
 		return _attributes[key] as T?
 	}
 	
-	override fun setNullable(key: Any, value: Any?) {
+	override fun setNullable(key: String, value: Any?) {
 		if (value == null)
 			_attributes.remove(key)
 		else
 			_attributes[key] = value
 	}
 	
-	override fun <FRONTEND : Any> getTyped(key: Any, codec: Codec<FRONTEND, Any>): FRONTEND? {
+	override fun <FRONTEND : Any> getTyped(key: String, codec: Codec<FRONTEND, Any>): FRONTEND? {
 		return getTyped<Any>(key)?.let { codec.read(it) }
 	}
 	
-	override fun <FRONTEND : Any> setNullable(key: Any, value: FRONTEND?, codec: Codec<FRONTEND, Any>) {
+	override fun <FRONTEND : Any> setNullable(key: String, value: FRONTEND?, codec: Codec<FRONTEND, Any>) {
 		return setNullable(key, value?.let { codec.write(it) })
 	}
 	
