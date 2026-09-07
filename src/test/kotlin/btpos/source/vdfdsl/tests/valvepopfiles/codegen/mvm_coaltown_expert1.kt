7e1bd2da3d2ee7c82e64213878a8ee59d2e0adf2
ByThePowerOfScience <16433721+ByThePowerOfScience@users.noreply.github.com>
2026-09-07 16:45:42 -0400

    Debugging: Add debug file outputs to mvm_coaltown_expert1 codegen test

--- src/test/kotlin/btpos/source/vdfdsl/tests/valvepopfiles/codegen/mvm_coaltown_expert1.kt
+++ src/test/kotlin/btpos/source/vdfdsl/tests/valvepopfiles/codegen/mvm_coaltown_expert1.kt
@@ -3,6 +3,8 @@ package btpos.source.vdfdsl.tests.valvepopfiles.codegen
 import btpos.source.vdfdsl.codegen.WeirdMutableIterableSubtree
 import btpos.source.vdfdsl.types.WaveSchedule
 import btpos.source.vdfdsl.vdfparser.ParseVDF
+import java.io.File
+import java.io.PrintStream
 import kotlin.test.Test
 import kotlin.test.assertNotNull
 
@@ -10,6 +12,8 @@ class mvm_coaltown_expert1 {
 	@Test
 	fun doCodegen() {
 		System.setProperty("vdfdsl.codegen", "true")
+		System.setOut(PrintStream(File("console.log").outputStream()))
+		System.setErr(PrintStream(File("err.log").outputStream()))
 		val popfile = ParseVDF.parse(ClassLoader.getSystemResourceAsStream("mvm_coaltown_expert1.pop")!!)
 		val codegen = WaveSchedule.CODEGEN_TYPE.get().decode(WeirdMutableIterableSubtree(null, popfile))
 		assertNotNull(codegen)
